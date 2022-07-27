68b52d5b1f05368bc82030976606e5c31f9b4fb4
Tim <dalytimdaly@gmail.com>
2022-07-27 17:39:47 -0600

    add Rock Dodger

--- script.js
+++ script.js
@@ -8,4 +8,48 @@ const click = document.getElementById('banana');
 click.addEventListener('click', function() {
     alert('Heeeey, keep it PG folks!');
 });
+
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0 ) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener ("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener ("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerUp () {
+    const upNumbers = dodger.style.up.replace("px", "");
+    const up = parseInt(upNumbers, 10);
+
+    if (up < 360) {
+        dodger.style.up = `${up + 1}px`
+    }
+
+    
+}
 }
